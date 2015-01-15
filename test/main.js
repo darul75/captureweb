9d30e2129a3bd90bc1142dd1cf8d726c5f9dfc31
julien <darul75@gmail.com>
2015-01-15 19:40:40 +0100

    test + phantom lib

--- test/main.js
+++ test/main.js
@@ -3,32 +3,32 @@ var captureweb = require('../src/captureweb');
 var assert = require("assert");
 
 describe('service calls', function() {
-    describe('default render when start', function() {
+	
+	describe('default render when start', function() {
+		this.timeout(30000);
 		it('arguments', function(done) {
 			captureweb.capture({url: 'https://github.com',
 				mime: 'image/png',
 				viewportSize: { width: 1280, height: 1024 } }, function(err, stream) {
-				if (err) {
-					console.log(err);
-					return;          
-				}
+					if (err) {
+						console.log(err);
+						return;          
+					}
 
-				var data = '';
+					var data = '';
 
-				stream.on('data', function(d){
-					data += d;
+					stream.on('data', function(d){
+						data += d;
+					});
+
+					stream.on('end', function(){
+						console.log('ok stream is here');
+						assert.equal(1, 1);
+						done();
+					});
+
 				});
-
-				stream.on('end', function(){
-					console.log('ok stream is here');
-					assert.equal(1, 1);
-					done();
-				});
-
-			});
-
-			this.timeout(19000);
+			
 		});
 	});
 });
-		
